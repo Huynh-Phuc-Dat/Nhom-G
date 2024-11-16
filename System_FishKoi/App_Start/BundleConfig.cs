844c14f0e164f9d7192f4bd3ab918b3f42d8c59f
Đạt <datmt3699@gmail.com>
2024-11-16 15:21:39 +0700

    yeu cau

--- System_FishKoi/App_Start/BundleConfig.cs
+++ System_FishKoi/App_Start/BundleConfig.cs
@@ -5,6 +5,7 @@ namespace System_FishKoi
 {
     public class BundleConfig
     {
+        //
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
