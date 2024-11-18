1f5ba6dc0262df5c9d4f0251c33295a79792bc87
Đạt <datmt3699@gmail.com>
2024-11-18 09:51:07 +0700

    Bảo

--- System_FishKoi/Areas/Client_User/Client_UserAreaRegistration.cs
+++ System_FishKoi/Areas/Client_User/Client_UserAreaRegistration.cs
@@ -12,6 +12,7 @@ namespace System_FishKoi.Areas.Client_User
             }
         }
 
+
         public override void RegisterArea(AreaRegistrationContext context)
         {
             context.MapRoute("Client_User_Client_User_Client_User", "thanh-vien-dang-ky", new { area = "Client_User", controller = "Client_User", action = "Client_User", id = UrlParameter.Optional });
